8c99edc5c9c67e1622bd95a1cb2de1cde375a09a
rosalinda <vicesarosalinda@gmail.com>
2022-02-25 20:54:34 +0700

    Default Changelist

---
+++ android/app/src/main/kotlin/com/example/percobaan_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.percobaan_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  String _text = "Prima : ";
+  int x = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+      _text = "prima : ";
+
+      for(int i=0; i<=_counter; i++){
+        for (int j=1; j<=i; j++){
+          if (i%j==0){
+            x++;
+          }
+        }
+        if (x==2){
+          _text += '${i}, ';
+        }
+        x=0;
+      }
+      // if(_counter%2==0 && _counter%3==0){
+      //   _text += "${_counter}, ";
+      // }
+      // if(_counter>10) {
+      //   // _counter = 1;
+      //   _counter = 0;
+      // }
+      // if(_counter%2 == 0) {
+      //   _text = "Genap";
+      // } else{
+      //   _text = "Ganjil";
+      // }
+      // _text = "Ganjil: ";
+      // for(int i=0; i<=_counter; i++){
+      //   if(i%2 != 0){
+      //     _text += '${i},';
+      //   }
+      // }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            Text(
+              _text,
+              style: Theme.of(context).textTheme.headline4,
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
